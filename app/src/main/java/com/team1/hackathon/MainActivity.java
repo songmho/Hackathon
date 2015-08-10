683c3452163c142ca716bdfa5171f4acec72029c
eugene <7yjlove7@naver.com>
2015-08-11 02:25:46 +0900

    splash 화면 추가, library  추가

--- app/src/main/java/com/team1/hackathon/MainActivity.java
+++ app/src/main/java/com/team1/hackathon/MainActivity.java
@@ -1,5 +1,6 @@
 package com.team1.hackathon;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
@@ -11,6 +12,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        startActivity(new Intent(this, SplashActivity.class));
     }
 
     @Override

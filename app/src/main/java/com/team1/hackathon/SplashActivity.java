debd8250f0e58b46575b1d9108adc132ce33ab85
eugene <7yjlove7@naver.com>
2015-08-11 02:32:41 +0900

    splash 화면 추가, library  추가

--- app/src/main/java/com/team1/hackathon/SplashActivity.java
+++ app/src/main/java/com/team1/hackathon/SplashActivity.java
@@ -20,6 +20,6 @@ public class SplashActivity extends Activity {
                     finish();
                 }
             };
-            handler.sendEmptyMessageDelayed(0,2000);
+            handler.sendEmptyMessageDelayed(0,1500);
         }
     }
